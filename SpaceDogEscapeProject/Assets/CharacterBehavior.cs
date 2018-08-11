acbe0b9dfb1288b8cac01a5350f599562f73a2e5
yayapipi <yayapipi@hotmail.com>
2018-08-12 00:34:31 +0800

    Push

---
+++ SpaceDogEscapeProject/Assets/CharacterBehavior.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class CharacterBehavior : MonoBehaviour {
+    private Animator anim;
+    Vector2 force;
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime*5f, 0, 0);
+        if (Input.GetButtonDown("Jump"))
+        {
+            anim.SetBool("isJump", true);
+            Vector2 a = new Vector2(0, 350);
+            GetComponent<Rigidbody2D>().AddForce(a);
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        if (Input.GetAxis("Horizontal")<0)
+        {
+            anim.SetBool("isRun", true);
+            GetComponent<SpriteRenderer>().flipX = true;
+            
+        }
+        else if (Input.GetAxis("Horizontal") > 0)
+        {
+            anim.SetBool("isRun", true);
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else
+        {
+            anim.SetBool("isRun", false);
+            anim.SetBool("isJump", false);
+        }
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "RedTrigger")
+        {
+            Debug.Log("Crush");
+            Destroy(collision.transform.parent.gameObject);
+        }
+
+    }
+
+ 
+}
---
+++ SpaceDogEscapeProject/Assets/LevelMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	public float speed = 2f;
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0,0,speed * Time.deltaTime);
+    }
+
+   
+}
